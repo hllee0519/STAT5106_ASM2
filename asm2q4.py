cb8ea9445a958df0e7a97b97ed4a7af00996d793
le279466 <leehl@hongyip.com>
2022-09-30 11:40:26 +0800

    finish q4

--- asm2q4.py
+++ asm2q4.py
@@ -2,6 +2,11 @@ import re
 
 for line in open("dept_hist.html", "r",encoding="utf-8"):  # opened in text-mode; all EOLs are converted to '\n'
     line = line.rstrip('\n')
-    searchObj = re.search( r'.*?(Prof.*? [^A-Z][a-z$]+).*?chair.*?.*?', line, re.M)
-    if searchObj:
-        print(searchObj.group(1))
+    searchName = re.search( r'(Prof\.|Professor)\s([A-Z][a-z]+[-a-z]+|[A-Z][a-z]+)\s([A-Z][a-z]+\s[A-Z][a-z]+|[A-Z][a-z]+) .*? chair.*?', line)
+    searchName2 = re.search( r'.*? chair.*? (Prof\.|Professor)\s([A-Z][a-z]+[-a-z]+)\s([A-Z][a-z]+\s[A-Z][a-z]+|[A-Z][a-z]+) ', line)
+    try:
+        print(searchName.group(1), end=' ')
+        print(searchName.group(2), end=' ')
+        print(searchName.group(3))
+    except:
+        continue
--- testQ4.py
+++ testQ4.py
@@ -1,11 +1,18 @@
 import re
  
-line = "Professor Sik-yum Lee assumed the duty of chairman of the Department.";
+lines = ["Professor Sik-yum Lee assumed the duty of chairman of the Department.", 
+   "Professor Nai Ng Chan became the third chairman of the Department.",
+   "Foundation of the Department. 17 students were enrolled in the first year with Prof. Howell Tong as the chairman",
+   "Professor Qi Man Shao was inaugurated as the Department chairman"];
  
-searchObj = re.search( r'(Prof.*? [A-Z]([a-z])+) .*? chair.*? .*?', line, re.M|re.I)
- 
-if searchObj:
-   print("searchObj.group() : ", searchObj.group())
-   print( "searchObj.group(1) : ", searchObj.group(1))
-else:
-   print ("Nothing found!!")
+for line in lines:
+   searchObj = re.search( r'(Prof\.|Professor)\s([A-Z][a-z]+[-a-z]+|[A-Z][a-z]+)\s([A-Z][a-z]+\s[A-Z][a-z]+|[A-Z][a-z]+) .*? chair.*?', line)
+
+   try:
+      print( "searchObj.group() : ", searchObj.group())
+      print( "searchObj.group(1) : ", searchObj.group(1))
+      print( "searchObj.group(2) : ", searchObj.group(2))
+      print( "searchObj.group(3) : ", searchObj.group(3))
+      print( "searchObj.group(4) : ", searchObj.group(4))
+   except:
+      continue
